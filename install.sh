b20379497bede14b0ad12dd779ef0484cb34fcc0
Cyril Rohr <cyril.rohr@gmail.com>
2021-12-14 08:15:30 +0000

    install for ubuntu20

--- install.sh
+++ install.sh
@@ -5,18 +5,17 @@ set -o pipefail
 MY_USER=crohr
 MY_HOME=/home/$MY_USER
 
-mkdir -p $MY_HOME/.ssh
-chmod 0700 $MY_HOME/.ssh
-curl -L https://github.com/crohr.keys -o $MY_HOME/.ssh/authorized_keys
-chmod 0600 $MY_HOME/.ssh/authorized_keys
-chown -R crohr:crohr $MY_HOME/.ssh
-
 sed -i "s|PasswordAuthentication yes|PasswordAuthentication no|" /etc/ssh/sshd_config
 sshd -t
 service ssh restart
 
 id $MY_USER || useradd -d $MY_HOME --shell /bin/bash -m $MY_USER
 echo 'crohr    ALL = NOPASSWD: ALL' > /etc/sudoers.d/crohr
+mkdir -p $MY_HOME/.ssh
+chmod 0700 $MY_HOME/.ssh
+curl -L https://github.com/crohr.keys -o $MY_HOME/.ssh/authorized_keys
+chmod 0600 $MY_HOME/.ssh/authorized_keys
+chown -R crohr:crohr $MY_HOME/.ssh
 
 workspace=$(mktemp -d)
 
@@ -51,35 +50,30 @@ add-apt-repository -y ppa:martin-frost/thoughtbot-rcm
 echo 'deb http://download.virtualbox.org/virtualbox/debian yakkety contrib' > /etc/apt/sources.list.d/virtualbox.list
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 
-# docker-gc
-wget -qO - https://deb.packager.io/key | apt-key add -
-echo "deb https://deb.packager.io/gh/pkgr/docker-gc xenial pkgr" | tee /etc/apt/sources.list.d/docker-gc.list
-
 apt-get update -qq
 apt-get install -y \
 	docker-ce \
-	docker-gc \
-	rcm \
-	virtualbox-dkms \
-	virtualbox
+	rcm
+	# virtualbox-dkms \
+	# virtualbox
 
 getent group docker || groupadd docker
 usermod -aG docker $MY_USER
 
 # Install vagrant
-LATEST_VAGRANT_URL=$(curl -s https://releases.hashicorp.com/vagrant/index.json \
-    | jq --raw-output \
-        '.versions
-        | with_entries(select(.key | test("-rc[0-9]+$") | not))
-        | to_entries
-        | max_by(.key)
-        | .value.builds[]
-        | select(.arch=="x86_64")
-        | select(.os=="debian")
-        | .url')
-
-curl "$LATEST_VAGRANT_URL" -o $workspace/vagrant.deb
-dpkg -i $workspace/vagrant.deb
+# LATEST_VAGRANT_URL=$(curl -s https://releases.hashicorp.com/vagrant/index.json \
+#     | jq --raw-output \
+#         '.versions
+#         | with_entries(select(.key | test("-rc[0-9]+$") | not))
+#         | to_entries
+#         | max_by(.key)
+#         | .value.builds[]
+#         | select(.arch=="x86_64")
+#         | select(.os=="debian")
+#         | .url')
+#
+# curl "$LATEST_VAGRANT_URL" -o $workspace/vagrant.deb
+# dpkg -i $workspace/vagrant.deb
 
 # outside of su so that we can use ssh agent forwarding
 test -d $MY_HOME/.dotfiles || git clone git@github.com:crohr/dotfiles $MY_HOME/.dotfiles
@@ -94,8 +88,8 @@ rm -rf $MY_HOME/.vim/bundle/Vundle.vim
 git clone https://github.com/gmarik/Vundle.vim.git $MY_HOME/.vim/bundle/Vundle.vim
 rcup -x install.sh
 vim +PluginInstall +qall
-echo 'Host *
-  PubkeyAcceptedKeyTypes=+ssh-dss' > $MY_HOME/.ssh/config
+#echo 'Host *
+#  PubkeyAcceptedKeyTypes=+ssh-dss' > $MY_HOME/.ssh/config
 "
 
 # After that, move over the following private stuff:
